a4a5b3c44a70e03564e3096123a528dfe553a1af
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-04-25 17:54:50 -0400

    added beginning prompt

--- server.js
+++ server.js
@@ -3,3 +3,52 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 const db = require("./db/connection");
 
+// Starts prompts
+function startItUp() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "selection",
+      message: "Would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        "Add Employee",
+        "Remove Employees",
+        "Update Employee Role",
+        "Add Role",
+        "End",
+      ],
+    })
+    .then(function ({ selection }) {
+      switch (selection) {
+        case "View Employees":
+          viewEmployees();
+          break;
+
+        case "View Employees by Department":
+          viewEmployeesByDepartment();
+          break;
+
+        case "Add Employee":
+          insertEmployee();
+          break;
+
+        case "Remove Employees":
+          dropEmployees();
+          break;
+
+        case "Update Employee Role":
+          updateRole();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "End":
+          connection.end();
+          break;
+      }
+    });
+}
