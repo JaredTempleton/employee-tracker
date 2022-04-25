747ed32b00518279284764f444f1d30ab99c55c1
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-04-25 17:09:23 -0400

    added new database with logic in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+-- Creates the database for the employee tracker and will drop it if one exists already --
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+USE employeeTracker;
+
