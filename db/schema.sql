9f2c80815795846975a158d3eb91a53d9c14bc25
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-04-25 17:17:25 -0400

    created tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,23 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 USE employeeTracker;
 
+-- Table creation --
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    job_title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,2) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
